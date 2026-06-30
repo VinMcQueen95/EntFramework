ecb700519116e358a38084fec6e756e2fa64f2ff
Vinoy Mereddy <vinoy.mereddy@hcl.software>
2026-06-30 00:34:26 -0700

    made a change to error message

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -34,7 +34,7 @@ async clickLoginButton(){
    await this.page.getByRole('button', { name: 'Log in' })
    .click()
    .catch((error) => {
-       console.error('Error clicking login button and faced an error: $(error)'); 
+       console.error('Error clicking login button and faced an error and outputting the error: $(error)'); 
        throw error; 
    });
 
